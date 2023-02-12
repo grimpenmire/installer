794d2c19aac3ba3425b8c6bba64780d9e2198728
Grimpen Mire <miregrimpen@gmail.com>
2023-02-12 21:13:32 +0100

    Add DoH endpoint

--- install.sh
+++ install.sh
@@ -199,6 +199,10 @@ http {
             proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         }
 
+        location /${SECRET}/dns-cf {
+            proxy_pass https://cloudflare-dns.com/dns-query;
+        }
+
         location / {
             proxy_pass http://127.0.0.1:80;
         }
